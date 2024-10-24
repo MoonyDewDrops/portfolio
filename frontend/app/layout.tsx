4a6d655d457347b84ef2968884d74d71b91115b5
MoonyDewDrops <julia.brouwervanoudshoorn@gmail.com>
2024-10-24 15:30:44 +0200

    favicon fix

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -16,8 +16,13 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Julia Brouwer portfolio",
+  title: "Portfolio JB",
   description: "Julia Brouwer haar Portfolio",
+  // Add the favicon links in the metadata
+  icons: {
+    icon: "/favicon-32x32.png",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
@@ -27,6 +32,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
         <main>{children}</main>
